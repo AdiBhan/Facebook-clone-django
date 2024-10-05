0c028d09eed4efdf654f774a1602599495f5667a
AdiBhan <abhan03@bu.edu>
2024-10-05 07:37:29 -0400

    Added more comments for mini_fb assignment 5

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=30)
+    # Profile model for mini_fb. Contains: first_name, last_name, city , email_address & profile_image_url
+    first_name = models.CharField(max_length=30)  
     last_name = models.CharField(max_length=30)
     city = models.CharField(max_length=50)
     email_address = models.EmailField()
-    profile_image_url = models.URLField(max_length=200)
+    profile_image_url = models.URLField(max_length=200) 
     
+    
+    # Defined special print method for debugging purposes (when I created records)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -7,6 +7,6 @@ from django.views.generic import ListView
 
 class ShowAllProfilesView(ListView):
     ''' a view to show all Profiles from template html file show_all_profiles'''
-    model = Profile
-    template_name = 'mini_fb/show_all_profiles.html'
-    context_object_name = 'profiles'
+    model = Profile ## references model found in models.py
+    template_name = 'mini_fb/show_all_profiles.html'  # references HTML webpage to display UI of webpage found in templates/mini_fb/show_all.html
+    context_object_name = 'profiles' # profiles references the actual DB table which we can use in the HTML page to display records
